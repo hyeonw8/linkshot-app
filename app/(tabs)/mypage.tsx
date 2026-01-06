212ec07dc704dcca355ebe1bbb1fe33c68cbff4d
hyeonw8 <whgusrud223@gmail.com>
2026-01-06 23:56:30 +0900

    ✨ feat(mypage): 마이페이지 스크린 초기 구현

---
+++ app/(tabs)/mypage.tsx
@@ -0,0 +1,10 @@
+// app/(tabs)/mypage.tsx
+import { View, Text } from "react-native";
+
+export default function MyPageScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>👤 My Page Screen</Text>
+    </View>
+  );
+}
