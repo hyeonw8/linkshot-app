a2046b8e33f5ca0b71ef7e21432c3a9c872ec7cd
hyeonw8 <whgusrud223@gmail.com>
2026-01-07 23:59:46 +0900

    ✨ feat(auth): Google 로그인 초기 설정 및 인증 초기화 로직 추가
    - RootLayout: GoogleSignin.configure 설정 추가
    - expo-web-browser: maybeCompleteAuthSession 호출
    - AuthInitializer: 앱 진입 시 세션 복원 처리

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,28 @@
+import AuthInitializer from "@/features/auth/AuthInitializer";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { Stack } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import { useEffect } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function RootLayout() {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID!,
+      iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID!,
+    });
+  }, []);
+
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <>
+      <AuthInitializer />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </>
   );
 }
